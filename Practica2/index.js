4280cf46bbbdfc4288982c259d36cb67507f4d16
zFlayn <pablo.capistran123@gmail.com>
2022-02-19 09:32:41 -0600

    Modificaciones Practica 2
    Se agrego lo que faltaba de mostrar los productos como si fuese un ticket

--- Practica2/index.js
+++ Practica2/index.js
@@ -1,9 +1,8 @@
 console.log("hola mundo!");
 //Paso 1: Calcular totalvar total = 0;
 
-var descripcion;
+productos = [];
 var total = 0;
-var monto = 0;
 
 var montoTxt = document.getElementById("monto");
 
@@ -11,21 +10,28 @@ var totalMsg = document.getElementById("totalMsg");
 
 var descTxt = document.getElementById("descripcion");
 
-var ticket = document.getElementById("ticket")
+var ticket = document.getElementById("ticket");
 
-function mostrarProducto() {
-    descripcion = descTxt.value;
-    ticket.innerHTML = "Producto: " + descripcion
-}
 
 function calcularTotal() {
     //obtener el monto del campo de texto    monto = montoTxt.value;
+    var monto = 0;
+    var producto = descTxt.value;
     monto = montoTxt.value;
-    monto = parseInt(monto); //resolviendo conflicto de ionic -- revisar error    total = total + monto;
+    monto = parseInt(monto); //resolviendo conflicto de ionic -- revisar error    total = total + monto;    
+    
+    productos.push({producto, monto});
 
     total = total + monto;
     console.log(total);
+    console.log(productos);
 
+
+    ticket.innerHTML = "Productos : "
+    productos.forEach(item => {
+        ticket.innerHTML += "<br><ion-item><ion-label>"+item.producto+" $"+item.monto+"</ion-label></ion-item>"
+    });
     //Imprimir en pagina
+
     totalMsg.innerHTML = "Total : " + total;
 }
